ce8476f50b7fbda17fd2bdddfb81d9116700fce1
Q市橋 <ichihashi@q-co.jp>
2020-02-13 19:37:04 +0900

    一旦

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,22 @@
+const $ = require("jquery")
+// import $ from "jquery"
+
 const babel = () => {
     console.log("hello World")
+    console.dir($);
 }
+
+$(function() {
+    $("#btn").click(function() {
+        $(this).after('<div>テスト</div>');
+    });
+    $("div").click(function() {
+        $(this).css('background', 'red');
+    });
+    $(".test").on("click", () =>{
+        console.log("test");
+        window.alert("hello world");
+    })
+});
 
 babel()
