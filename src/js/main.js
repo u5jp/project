febc4a59f7cb3b4d1ef869dc341e08a9d801d80f
Q市橋 <ichihashi@q-co.jp>
2020-04-07 16:58:36 +0900

    ie対応のためcss読み込み位置変更

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,7 @@ import $ from "jquery"
 // import "slick-carousel"
 // import "../../node_modules/slick-carousel/slick/slick.css"
 // import "../../node_modules/slick-carousel/slick/slick-theme.css"
-import '../../dist/css/crazy/index.css';
+//import '../../dist/css/crazy/index.css';
 import crazy from './modules/crazy.js';
 
 const babel = () => {
